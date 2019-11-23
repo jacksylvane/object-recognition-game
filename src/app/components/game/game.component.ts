5d71a802fbdd9564caab34d4042f7cde3c4cf004
jacksylvane <sisakm5@gmail.com>
2019-11-24 00:27:07 +0100

    Add basic game functionality

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as mobilenet from '@tensorflow-models/mobilenet';
 
 @Component({
   selector: 'app-game',
@@ -6,10 +7,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-
+  probability: any = 'N/A';
+  correctAnswer: any;
+  imageClasified = false;
+  showResult = false;
+  possibleAnswers = [];
   constructor() { }
 
   ngOnInit() {
+    const img = document.getElementById('img');
+    this.clasifyImage(img).then(predictions => {
+      console.log('Predictions: ');
+      console.log(predictions);
+      this.correctAnswer = predictions[0];
+      this.probability = (predictions[0].probability * 100).toFixed(0);
+      this.imageClasified = true;
+      predictions.map(prediction => {
+        this.possibleAnswers.push({
+          name: prediction.className,
+          probability: prediction.probability,
+          correct: false
+        });
+      });
+    });
+
+
+    // setInterval(() => {
+    //   if (this.blurredIndex > 0) {
+    //     this.blurredIndex -= 0.1;
+    //   }
+    // }, 100);
   }
 
+  async clasifyImage(img) {
+    // Load the model.
+    const model = await mobilenet.load();
+
+    // Classify the image.
+    return await model.classify(img);
+  }
+  selectAnswer(answer) {
+    console.log('You have selected', answer);
+    if (answer === this.correctAnswer.className) {
+      this.possibleAnswers = this.possibleAnswers.map(x => {
+        return x.name === answer ? { ...x, correct: true } : x;
+      });
+    }
+    this.showResult = true;
+    console.log(this.possibleAnswers);
+
+  }
 }
--- src/app/components/screen/screen.component.ts
+++ src/app/components/screen/screen.component.ts
@@ -1,43 +1,20 @@
 import { Component, OnInit } from '@angular/core';
-// import * as tf from '@tensorflow/tfjs';
 
-import * as mobilenet from '@tensorflow-models/mobilenet';
 @Component({
   selector: 'app-screen',
   templateUrl: './screen.component.html',
   styleUrls: ['./screen.component.scss']
 })
 export class ScreenComponent implements OnInit {
-  blurredIndex = 0;
-  prediction = 'detecting...';
-  probability: any = 'N/A';
+  blurredIndex = 15;
   constructor() { }
 
   ngOnInit() {
-    const img = document.getElementById('img');
-    this.clasifyImage(img).then(predictions => {
-      console.log('Predictions: ');
-      console.log(predictions);
-      this.prediction = predictions[0].className;
-      this.probability = (predictions[0].probability * 100).toFixed(0);
-
-    });
-
-
-    // setInterval(() => {
-    //   if (this.blurredIndex > 0) {
-    //     this.blurredIndex -= 0.1;
-    //   }
-    // }, 100);
+    setInterval(() => {
+      if (this.blurredIndex > 0) {
+        this.blurredIndex -= 0.3;
+      }
+    }, 100);
   }
-
-  async clasifyImage(img) {
-    // Load the model.
-    const model = await mobilenet.load();
-
-    // Classify the image.
-    return await model.classify(img);
-  }
-
 
 }
